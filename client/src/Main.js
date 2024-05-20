cb1248d335dfe67fc009414a088d35b57f7ff874
Tamha10 <136102681+Tamha10@users.noreply.github.com>
2024-05-20 01:00:53 -0400

    Update Main.js

--- client/src/Main.js
+++ client/src/Main.js
@@ -39,23 +39,50 @@ function Main({ token, playlistName, setPlaylistName, genre, setGenre, target_ac
 
     return (
         <div className="main">
-            <div className="controls">
-                    <input type="text" value={playlistName} onChange={handleName}/>
-                    <input type="text" value={genre} onChange={handleGenre}/>
-                    <input type="range" min="0" max="1" step="0.01" value={target_acousticness} onChange={handleAcousticness} />
-                    <input type="range" min="0" max="1" step="0.01" value={target_danceability} onChange={handleDanceability} />
-                    <input type="range" min="0" max="1" step="0.01" value={target_energy} onChange={handleEnergy} />
-                    <input type="range" min="0" max="1" step="0.01" value={target_instrumentalness} onChange={handleInstrumentalness} />
-                    <input type="range" min="0" max="1" step="0.01" value={target_loudness} onChange={handleLoudness} />
-                    <input type="range" min="0" max="200" step="0.1" value={target_tempo} onChange={handleTempo} />
-                    <input type="range" min="0" max="1" step="0.01" value={target_valence} onChange={handleValence} />
-                    <button onClick={handleCreatePlaylist}>Create Playlist</button>
-            </div>
-            <div className="playlist-cards">
-            </div>
+          <div className="controls">
+            <label>
+              Playlist Name:
+              <input type="text" value={playlistName} onChange={(e) => setPlaylistName(e.target.value)} />
+            </label>
+            <label>
+              Genre:
+              <input type="text" value={genre} onChange={(e) => setGenre(e.target.value)} />
+            </label>
+            <label>
+              Acousticness:
+              <input type="range" min="0" max="1" step="0.01" value={target_acousticness} onChange={(e) => setAcousticness(e.target.value)} />
+            </label>
+            <label>
+              Danceability:
+              <input type="range" min="0" max="1" step="0.01" value={target_danceability} onChange={(e) => setDanceability(e.target.value)} />
+            </label>
+            <label>
+              Energy:
+              <input type="range" min="0" max="1" step="0.01" value={target_energy} onChange={(e) => setEnergy(e.target.value)} />
+            </label>
+            <label>
+              Instrumentalness:
+              <input type="range" min="0" max="1" step="0.01" value={target_instrumentalness} onChange={(e) => setInstrumentalness(e.target.value)} />
+            </label>
+            <label>
+              Loudness:
+              <input type="range" min="0" max="1" step="0.01" value={target_loudness} onChange={(e) => setLoudness(e.target.value)} />
+            </label>
+            <label>
+              Tempo:
+              <input type="range" min="0" max="200" step="0.1" value={target_tempo} onChange={(e) => setTempo(e.target.value)} />
+            </label>
+            <label>
+              Valence:
+              <input type="range" min="0" max="1" step="0.01" value={target_valence} onChange={(e) => setValence(e.target.value)} />
+            </label>
+            <button onClick={handleCreatePlaylist}>Create Playlist</button>
+          </div>
+          <div className="playlist-cards">
+          </div>
         </div>
-    )
-}
+      );
+    }
 
 export default Main;
 
